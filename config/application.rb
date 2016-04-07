9cc250e5ff89f99ba6b5719eb42070a7a41ed85b
Mark Lohr <markuslohr@gmail.com>
2016-04-07 15:36:05 -0400

    setup Bower

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,6 @@ module ResearchMasterId
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
   end
 end
