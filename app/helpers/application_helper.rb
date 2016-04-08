0375e3cb6524a1727f990d213655c31c6f6500d5
William Holt <holtw@musc.edu>
2016-04-08 16:12:59 -0400

    added parens per ruby style guide

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 
-  def bootstrap_class_for flash_type
+  def bootstrap_class_for(flash_type)
     { success: "alert-success", error: "alert-danger", alert: "alert-danger", notice: "alert-success" }[flash_type.to_sym] || flash_type.to_s
   end
 
