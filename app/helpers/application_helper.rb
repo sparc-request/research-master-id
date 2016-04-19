cb8008ff2d2e5841816ccb04d2b686851e56431d
Mark Lohr <markuslohr@gmail.com>
2016-04-19 09:14:58 -0400

    adding navbar to homepage

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,12 @@ module ApplicationHelper
     { success: "alert-success", error: "alert-danger", alert: "alert-danger", notice: "alert-success" }[flash_type.to_sym] || flash_type.to_s
   end
 
+  def logged_in identity
+    content_tag(:span, "#{t(:navbar)[:logged_in_msg]} #{current_identity.full_name} (#{current_identity.email})", class: "logged-in-as", "aria-hidden" => "true")
+    puts "*" * 20
+    puts current_identity
+  end
+
   def flash_messages(opts = {})
     flash.each do |msg_type, message|
       concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} fade in") do
--- app/models/identity.rb
+++ app/models/identity.rb
@@ -3,4 +3,9 @@ class Identity < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
          :rememberable, :trackable
+
+  # def full_name
+  #   [first_name, last_name].join(' ')
+  # end
+
 end
