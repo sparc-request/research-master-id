eb1c34e860d5fafcbf53f5c332d47d278481a91a
William Holt <holtw@musc.edu>
2016-06-21 14:53:06 -0400

    added bootstrap table with json, added waitforajax method and installed launchy gem to open screenshots

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'haml-rails', '~> 0.9'
 gem 'devise'
 gem 'faker'
+gem 'launchy'
 
 
 
--- app/controllers/research_masters_controller.rb
+++ app/controllers/research_masters_controller.rb
@@ -1,6 +1,10 @@
 class ResearchMastersController < ApplicationController
 
   def index
-
+    @research_masters = current_user.research_masters
+    respond_to do |format|
+      format.html
+      format.json { render json: @research_masters }
+    end
   end
 end
