ef3c0e2073cf6d6bfc879807963af689b70d362b
William Holt <holtw@musc.edu>
2016-06-24 11:34:25 -0400

    wth - Database cleaner configuration
    I setup database_cleaner to clean sharded SPARC records. After each
    test, the test database is cleaned out which wasn't happening before.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -21,10 +21,12 @@ require 'rspec/rails'
 # require only the support files necessary.
 #
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Rails.application.eager_load!
 
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
+
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
@@ -33,6 +35,9 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
+  #
+
+  config.use_transactional_fixtures = false
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -1,58 +1,62 @@
 RSpec.configure do |config|
 
-  config.use_transactional_fixtures = false
-
-  config.before(:suite) do
-    if config.use_transactional_fixtures?
-
-      raise(<<-MSG)
-        Delete line `config.use_transactional_fixtures = true` from rails_helper.rb
-        (or set it to false) to prevent uncommitted transactions being used in
-        JavaScript-dependent specs.
-        During testing, the Ruby app server that the JavaScript browser driver
-        connects to uses a different database connection to the database connection
-        used by the spec.
-
-        This Ruby app server database connection would not be able to see data that
-        has been setup by the spec's database connection inside an uncommitted
-        transaction.
-        Disabling the use_transactional_fixtures setting helps avoid uncommitted
-        transactions in JavaScript-dependent specs, meaning that the Ruby app server
-        database connection can see any data set up by the specs.
-      MSG
-
-    end
-  end
-
+  # Clean data before running the suite
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
+    models = ActiveRecord::Base.descendants.select { |model| model.respond_to?(:sparc_record?) }
+    models.
+      each do |model|
+        DatabaseCleaner[:active_record, model: model].clean_with(:truncation)
+      end
   end
 
+  # Set default strategy to transaction
   config.before(:each) do
     DatabaseCleaner.strategy = :transaction
+    models = ActiveRecord::Base.descendants.select { |model| model.respond_to?(:sparc_record?) }
+    models.
+      each do |model|
+        DatabaseCleaner[:active_record, model: model].strategy = :transaction
+      end
   end
 
-  config.before(:each, type: :feature) do
-    # :rack_test driver's Rack app under test shares database connection
-    # with the specs, so we can use transaction strategy for speed.
-    driver_shares_db_connection_with_specs = Capybara.current_driver == :rack_test
-
-    if driver_shares_db_connection_with_specs
-      DatabaseCleaner.strategy = :transaction
-    else
-      # Non-:rack_test driver is probably a driver for a JavaScript browser
-      # with a Rack app under test that does *not* share a database
-      # connection with the specs, so we must use truncation strategy.
-      DatabaseCleaner.strategy = :truncation
-    end
+  # For js: true tests use the truncation strategy
+  config.before(:each, js: true) do |example|
+    DatabaseCleaner.strategy = :truncation
+    models = ActiveRecord::Base.descendants.select { |model| model.respond_to?(:sparc_record?) }
+    models.
+      each do |model|
+        DatabaseCleaner[:active_record, model: model].strategy = :truncation
+      end
   end
 
+  # For enqueue: false tests use the truncation strategy
+  config.before(:each, enqueue: false) do |example|
+    DatabaseCleaner.strategy = :truncation
+    models = ActiveRecord::Base.descendants.select { |model| model.respond_to?(:sparc_record?) }
+    models.
+      each do |model|
+        DatabaseCleaner[:active_record, model: model].strategy = :truncation
+      end
+  end
+
+  # Start the cleaner to catch deletions in tests
   config.before(:each) do
     DatabaseCleaner.start
+    models = ActiveRecord::Base.descendants.select { |model| model.respond_to?(:sparc_record?) }
+    models.
+      each do |model|
+        DatabaseCleaner[:active_record, model: model].start
+      end
   end
 
-  config.after(:each) do
+  # Clean data post-test
+  config.append_after(:each) do
     DatabaseCleaner.clean
+    models = ActiveRecord::Base.descendants.select { |model| model.respond_to?(:sparc_record?) }
+    models.
+      each do |model|
+        DatabaseCleaner[:active_record, model: model].clean
+      end
   end
-
 end
