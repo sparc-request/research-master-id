3a90078ceac2383574a8bbc6711b5f7d76a0e706
William Holt <holtw@musc.edu>
2016-06-29 14:04:39 -0400

    setting up shards.yml on server

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,7 @@ set :log_level, :debug
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', '.ruby-version', '.ruby-gemset')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/shards.yml', '.ruby-version', '.ruby-gemset')
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system')
