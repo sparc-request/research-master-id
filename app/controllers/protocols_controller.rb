6fbb4b192225a232c5620df03ebf4b425f1f07e7
William Holt <holtw@musc.edu>
2016-07-01 14:27:55 -0400

    added pi name and department to table

--- app/controllers/protocols_controller.rb
+++ app/controllers/protocols_controller.rb
@@ -1,7 +1,7 @@
 class ProtocolsController < ApplicationController
   def index
     @q = Protocol.ransack(params[:q])
-    @protocols = @q.result(distinct: true).limit(10)
+    @protocols = @q.result(distinct: true).includes(:project_roles => :identity).limit(10)
     respond_to do |format|
       format.html
     end
--- app/models/protocol.rb
+++ app/models/protocol.rb
@@ -2,5 +2,14 @@ class Protocol < ActiveRecord::Base
   self.inheritance_column = nil # ignore STI
   include SparcShard
 
-  has_many :research
+  has_many :research_masters
+  has_many :project_roles
+
+  def primary_principal_investigator
+    primary_pi_project_role.try(:identity)
+  end
+
+  def primary_pi_project_role
+    project_roles.detect { |pr| pr.role == 'primary-pi' }
+  end
 end
