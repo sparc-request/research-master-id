ed0f61d06b8e02311186d64c5a13e04dd8613299
Jason Leonard <jleonardw9@gmail.com>
2016-07-07 14:44:35 -0400

    Protocols already associated with a RM record no longer included in the index.

--- app/controllers/protocols_controller.rb
+++ app/controllers/protocols_controller.rb
@@ -1,7 +1,8 @@
 class ProtocolsController < ApplicationController
   def index
     @q = Protocol.ransack(params[:q])
-    @protocols = @q.result(distinct: true).includes(:project_roles => :identity).limit(10)
+    associated_ids = AssociatedRecord.associated_protocol_ids
+    @protocols = @q.result(distinct: true).includes(:project_roles => :identity).limit(10).reject{ |x| associated_ids.include?(x.id) }
     respond_to do |format|
       format.html
     end
