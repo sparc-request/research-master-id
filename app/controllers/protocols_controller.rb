b334843c9932a5968ba4a520a21fdd1fb9c9b0b4
Jason Leonard <jleonardw9@gmail.com>
2016-07-08 09:36:11 -0400

    Making filter more readable.

--- app/controllers/protocols_controller.rb
+++ app/controllers/protocols_controller.rb
@@ -2,7 +2,7 @@ class ProtocolsController < ApplicationController
   def index
     @q = Protocol.ransack(params[:q])
     associated_ids = AssociatedRecord.associated_protocol_ids
-    @protocols = @q.result(distinct: true).includes(:project_roles => :identity).limit(10).reject{ |x| associated_ids.include?(x.id) }
+    @protocols = @q.result(distinct: true).includes(:project_roles => :identity).limit(10).reject{ |protocol| associated_ids.include?(protocol.id) }
     respond_to do |format|
       format.html
     end
