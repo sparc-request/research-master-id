e245f43ef7ea3d81b935178c10078b2bc6c33eb5
Jason Leonard <jleonardw9@gmail.com>
2016-07-12 11:34:38 -0400

    Small refactor to still return 10 records while also rejecting associated records.

--- app/controllers/protocols_controller.rb
+++ app/controllers/protocols_controller.rb
@@ -1,8 +1,8 @@
 class ProtocolsController < ApplicationController
   def index
-    @q = Protocol.ransack(params[:q])
     associated_ids = AssociatedRecord.associated_protocol_ids
-    @protocols = @q.result(distinct: true).includes(:project_roles => :identity).limit(10).reject{ |protocol| associated_ids.include?(protocol.id) }
+    @q = Protocol.where.not(id: associated_ids).ransack(params[:q])
+    @protocols = @q.result(distinct: true).includes(:project_roles => :identity).limit(10)
     respond_to do |format|
       format.html
     end
