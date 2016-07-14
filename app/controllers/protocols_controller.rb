57b3cf0363e2ab8e57df4c55c5eac90545d3d425
William Holt <holtw@musc.edu>
2016-07-14 10:57:58 -0400

    retrieving protocol information from api

--- app/controllers/protocols_controller.rb
+++ app/controllers/protocols_controller.rb
@@ -1,8 +1,8 @@
 class ProtocolsController < ApplicationController
   def index
     associated_ids = AssociatedRecord.associated_protocol_ids
-    @q = Protocol.where.not(id: associated_ids).ransack(params[:q])
     @protocols = HTTParty.get('http://localhost:3000/protocols')
+    @q = @protocols.reject { |k, v| associated_ids.include?(k['id']) }
     respond_to do |format|
       format.html
     end
