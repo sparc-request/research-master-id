aea1b0b677654107d7e894d257c801c1f6e98197
William Holt <holtw@musc.edu>
2016-07-15 13:13:20 -0400

    adding in eirb records

--- app/controllers/protocols_controller.rb
+++ app/controllers/protocols_controller.rb
@@ -1,13 +1,21 @@
 class ProtocolsController < ApplicationController
   def index
     associated_ids = AssociatedRecord.associated_protocol_ids
-    protocols = HTTParty.get('http://localhost:3000/protocols')
-    gon.protocols = protocols.reject { |k, v| associated_ids.include?(k['id']) }
-    if params[:search]
-      @protocols = params[:data]
-    else
-      @protocols = gon.protocols
-    end
+    get_protocols = HTTParty.get('http://localhost:3000/protocols')
+    protocols_array = JSON.parse(get_protocols.body)
+    get_eirb_studies = HTTParty.get('http://localhost:5000/studies.json')
+    eirb_studies_array = JSON.parse(get_eirb_studies.body)
+
+    protocols_hash = Hash[protocols_array.each_slice(2).to_a]
+
+    eirb_hash = Hash[eirb_studies_array.each_slice(2).to_a]
+
+    entire_protocol_hash = protocols_hash.merge(eirb_hash)
+
+    gon.protocols = entire_protocol_hash.reject { |k, v| associated_ids.include?(k['id']) }
+
+    @protocols = gon.protocols
+
     respond_to do |format|
       format.html
     end
