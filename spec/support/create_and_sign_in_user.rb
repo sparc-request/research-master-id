712b681d08affad53df17973fab3db68c12243c8
William Holt <holtw@musc.edu>
2016-12-08 09:14:35 -0500

    wth - Delete 'Look Up Protocols' button
    I deleted look up protocol button on the Research Masters page

--- spec/support/create_and_sign_in_user.rb
+++ spec/support/create_and_sign_in_user.rb
@@ -3,7 +3,7 @@ module CreateAndSignInUser
   def create_and_sign_in_user
     user = create(:user)
     visit new_user_session_path
-    fill_in 'user_email', with: user.email
+    fill_in 'user_login', with: user.email
     fill_in 'user_password', with: user.password
     click_button 'Log in'
   end
