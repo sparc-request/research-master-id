345076b0298027933c2ce025bf57712d6b212d4c
Jason Leonard <jleonardw9@gmail.com>
2017-03-09 08:02:11 -0600

    Needed to change the format of the regex.

--- app/models/research_master.rb
+++ app/models/research_master.rb
@@ -26,10 +26,10 @@ class ResearchMaster < ActiveRecord::Base
     message: 'There is an existing Research Master record with the same PI Name
     and Department' }
 
-  validates_format_of :long_title, with: /^[a-zA-Z\d\s]*$/, 
+  validates_format_of :long_title, with: /\A[a-zA-Z0-9]*\z/, 
     message: 'Special characters are not allowed in the Long Title'
 
-  validates_format_of :short_title, with: /^[a-zA-Z\d\s]*$/,
+  validates_format_of :short_title, with: /\A[a-zA-Z0-9]*\z/,
     message: 'Special characters are not allowed in the Short Title'
 
   accepts_nested_attributes_for :associated_record, allow_destroy: true,
