b467ded02e25907171c02581df69053059c92256
William Holt <holtw@musc.edu>
2017-06-09 12:07:32 -0400

    updated another migration for 5.1

--- db/migrate/20170509125759_merge_associated_records_and_research_master.rb
+++ db/migrate/20170509125759_merge_associated_records_and_research_master.rb
@@ -1,4 +1,4 @@
-class MergeAssociatedRecordsAndResearchMaster < ActiveRecord::Migration
+class MergeAssociatedRecordsAndResearchMaster < ActiveRecord::Migration[5.0]
   def change
     add_column :research_masters, :sparc_protocol_id, :integer, after: :user_id
     add_column :research_masters, :eirb_protocol_id, :integer, after: :sparc_protocol_id
