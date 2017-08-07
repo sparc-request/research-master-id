7a58f83c3d8b3d7678e84f39070abef79e00ef95
William Holt <holtw@musc.edu>
2017-08-07 10:00:32 -0400

    updating staging site

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,6 +2,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   skip_before_action :authenticate_user!
 
   def shibboleth
+    Rails.logger.debug(request.env['omniauth.auth'])
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
