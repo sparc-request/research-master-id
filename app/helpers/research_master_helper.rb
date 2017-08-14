4e0e9c3deab379a18540e72eac67808f74d9c1ad
William Holt <will.t.holt@gmail.com>
2017-08-14 15:42:17 -0400

    wth - (RMID) Add Required Field Indicator Asterisk (#112)
    After talking with Will, we should add an asterisk (*) indicator to labels to indicate when fields are required.
    
    [#149808918]
    
    Story - https://www.pivotaltracker.com/story/show/149808918

---
+++ app/helpers/research_master_helper.rb
@@ -0,0 +1,6 @@
+module ResearchMasterHelper
+
+  def mark_required(object, attribute)
+    "*" if object.class.validators_on(attribute).map(&:class).include? ActiveRecord::Validations::PresenceValidator
+  end
+end
