a70e78f36124e6a8b1f2b01a0761da6daf7a15d6
William Holt <holtw@musc.edu>
2017-08-14 11:48:58 -0400

    wth - (RMID) Add Controller Authorization to Edit/Update Actions
    I can remove the `disabled` class on the Edit Research Master button to access any Research Master. There should be authorization checks in the controller to prevent me from accessing it, even after removing the class.
    
    [#149809033]
    
    Story - https://www.pivotaltracker.com/story/show/149809033

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
+  rescue_from CanCan::AccessDenied do |exception|
+    respond_to do |format|
+      format.json { head :forbidden, content_type: 'text/html' }
+      format.html { redirect_to main_app.root_url, notice: exception.message }
+      format.js   { render js: "sweetAlert('Forbidden', 'You do not have permissions to do that', 'error')" }
+    end
+  end
+
   def test_exception_notifier
     raise 'This is a test. This is only a test.'
   end
