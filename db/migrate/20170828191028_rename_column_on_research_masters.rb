b9807f736fc1665d10d3eb3e90c6ca26a0784b99
William Holt <holtw@musc.edu>
2017-08-29 15:25:07 -0400

    update new column with old values

--- db/migrate/20170828191028_rename_column_on_research_masters.rb
+++ db/migrate/20170828191028_rename_column_on_research_masters.rb
@@ -1,6 +1,5 @@
 class RenameColumnOnResearchMasters < ActiveRecord::Migration[5.1]
   def change
-    remove_reference :research_masters, :user, index: true, foreign_key: true
     add_reference :research_masters, :creator, foreign_key: { to_table: :users }, after: :funding_source, type: :integer
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170828192145) do
+ActiveRecord::Schema.define(version: 20170829192308) do
 
   create_table "api_keys", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
     t.string "access_token"
