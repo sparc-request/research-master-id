09d68cc54a8555448c2f7b6b0e846fede30eecaa
Kyle Glick <glickk@musc.edu>
2018-09-10 10:59:34 -0500

    [#160263256] add eirb dates to api

--- app/controllers/api/research_masters_controller.rb
+++ app/controllers/api/research_masters_controller.rb
@@ -2,15 +2,17 @@ class Api::ResearchMastersController < Api::BaseController
 
   def index
     @research_masters = ResearchMaster.all
+
     respond_to do |format|
-      format.json { render json: @research_masters }
+      format.json { render json: @research_masters.map(&:to_json) }
     end
   end
 
   def show
     @research_master = ResearchMaster.find(params[:id])
+
     respond_to do |format|
-      format.json { render json: @research_master }
+      format.json { render json: @research_master.to_json }
     end
   end
 end
