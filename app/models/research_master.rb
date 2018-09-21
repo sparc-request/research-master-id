49075f2b193c5f9bcb25613627a5e02d62f666c1
Kyle Glick <glickk@musc.edu>
2018-09-21 11:02:13 -0500

    [#160263256] refactor research master JSON

--- app/models/research_master.rb
+++ app/models/research_master.rb
@@ -54,19 +54,4 @@ class ResearchMaster < ApplicationRecord
   def clinical_research?
     ['clinical_research_billable', 'clinical_research_non_billable'].include?(self.research_type)
   end
-
-  def to_json
-    json = self.attributes
-
-    if eirb_p = self.eirb_protocol
-      json.merge!({
-        eirb_pro_number:          eirb_p.eirb_id,
-        date_initially_approved:  eirb_p.date_initially_approved.try(:strftime, "%m/%d/%Y"),
-        date_approved:            eirb_p.date_approved.try(:strftime, "%m/%d/%Y"),
-        date_expiration:          eirb_p.date_expiration.try(:strftime, "%m/%d/%Y")
-      })
-    end
-
-    json
-  end
 end
