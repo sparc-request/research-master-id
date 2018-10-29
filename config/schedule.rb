7a15418493931efbb804e1b3c67c3fb822cee77b
Andrew Cates <amcates@gmail.com>
2018-10-29 07:41:45 -0400

    splitting tasks up

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,5 @@
 every 1.day, at: ['2:30 am', '6:30 am', '10:30 am', '2:30 pm', '6:30 pm', '10:30 pm'], :roles => [:app] do
-  rake 'update_data'
+  rake 'update_from_sparc'
+  rake 'update_from_eirb'
+  rake 'update_from_coeus'
 end
