e8981ae26370c2377e4a5d5835886528ae1ef201
Andrew Cates <amcates@gmail.com>
2018-10-29 07:49:24 -0400

    making sure they don't run at the same time

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,11 @@
 every 1.day, at: ['2:30 am', '6:30 am', '10:30 am', '2:30 pm', '6:30 pm', '10:30 pm'], :roles => [:app] do
   rake 'update_from_sparc'
+end
+
+every 1.day, at: ['2:35 am', '6:35 am', '10:35 am', '2:35 pm', '6:35 pm', '10:35 pm'], :roles => [:app] do
   rake 'update_from_eirb'
+end
+
+every 1.day, at: ['2:40 am', '6:40 am', '10:40 am', '2:40 pm', '6:40 pm', '10:40 pm'], :roles => [:app] do
   rake 'update_from_coeus'
 end
