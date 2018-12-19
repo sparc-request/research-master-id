49aedbf84609b976050c1d7a5bdb1708dc30709e
Kyle Glick <glickk@musc.edu>
2018-12-19 10:44:16 -0600

    add api/protocols_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
+
+  before_action :authenticate_user!, unless: -> { request.path_parameters[:controller].include?("api") }
 
   rescue_from CanCan::AccessDenied do |exception|
     respond_to do |format|
