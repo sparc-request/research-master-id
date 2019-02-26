2aa985613e6ef917877b61d50dfa742fd631a588
Jason Leonard <jleonardw9@gmail.com>
2019-02-26 12:21:28 -0600

    Validation of department not critical for now.

--- app/models/research_master.rb
+++ app/models/research_master.rb
@@ -15,8 +15,7 @@ class ResearchMaster < ApplicationRecord
 
   paginates_per 50
 
-  validates :department,
-    :long_title,
+  validates :long_title,
     :short_title,
     :funding_source,
     presence: true
@@ -24,17 +23,12 @@ class ResearchMaster < ApplicationRecord
   validates_length_of :short_title, maximum: 255
 
   validates :pi_id,
-    uniqueness: { scope: [:department, :long_title],
+    uniqueness: { scope: [:long_title],
     message: 'There is an existing Research Master record with the same
     Department and Long Title' }
 
-  validates :department,
-    uniqueness: { scope: [:pi_id, :long_title],
-    message: 'There is an existing Research Master record with the same PI Name
-    and Long Title' }
-
   validates :long_title,
-    uniqueness: { scope: [:pi_id, :department],
+    uniqueness: { scope: [:department],
     message: 'There is an existing Research Master record with the same PI Name
     and Department' }
 
