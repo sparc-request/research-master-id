fffecc4c5fa9c11f986a10051c5ea02638a85c81
Jason Leonard <jleonardw9@gmail.com>
2019-03-11 16:32:22 -0500

    Locking down department for current prism users.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190222145341) do
+ActiveRecord::Schema.define(version: 20190310201713) do
 
   create_table "api_keys", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
     t.string "access_token"
@@ -131,6 +131,7 @@ ActiveRecord::Schema.define(version: 20190222145341) do
     t.string "department"
     t.boolean "admin"
     t.boolean "developer"
+    t.boolean "current_prism_user"
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
