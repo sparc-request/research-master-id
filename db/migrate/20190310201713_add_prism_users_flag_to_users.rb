0b9ee449d7e33f487e37f1834e4169dcf6310b5e
Jason Leonard <jleonardw9@gmail.com>
2019-03-13 09:21:55 -0500

    Adding update script to migration.

--- db/migrate/20190310201713_add_prism_users_flag_to_users.rb
+++ db/migrate/20190310201713_add_prism_users_flag_to_users.rb
@@ -1,5 +1,17 @@
 class AddPrismUsersFlagToUsers < ActiveRecord::Migration[5.1]
   def change
     add_column :users, :current_prism_user, :boolean, after: :developer
+
+    coeus_api   = ENV.fetch("COEUS_API")
+    prism_users = HTTParty.get("#{coeus_api}/prism", timeout: 500, headers: {'Content-Type' => 'application/json'})
+    research_master_users = User.all
+
+    research_master_users.each do |research_master_user|
+      prism_user = prism_users.select {|user| user["netid"] == research_master_user.net_id }.first
+
+      if prism_user
+        research_master_user.update_attributes(current_prism_user: true)
+      end
+    end
   end
 end
