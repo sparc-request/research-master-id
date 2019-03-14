d4fff7b446b6fad905caa260f890df446f1404d3
Kyle Glick <glickk@musc.edu>
2019-03-14 13:25:13 -0500

    [#153071091] primary pi data migration

--- app/models/protocol.rb
+++ app/models/protocol.rb
@@ -1,7 +1,7 @@
 class Protocol < ApplicationRecord
   audited
   self.inheritance_column = nil
-  has_one :primary_pi
+  has_one :primary_pi, class_name: :user
   has_many :research_master_coeus_relations
   has_many :research_masters, through: :research_master_coeus_relations
 end
