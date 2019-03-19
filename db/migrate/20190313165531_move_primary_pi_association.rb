3b874cbb3f729e7a552d995e6edcd00c6fb4ac3a
Andrew Cates <amcates@gmail.com>
2019-03-19 09:35:16 -0400

    fixing migration

--- db/migrate/20190313165531_move_primary_pi_association.rb
+++ db/migrate/20190313165531_move_primary_pi_association.rb
@@ -6,9 +6,8 @@ class MovePrimaryPiAssociation < ActiveRecord::Migration[5.1]
   end
 
   def up
-    add_reference :protocols, :primary_pi, after: :long_title, index: true
-
-    ActiveRecord::Base.transacation do
+    ActiveRecord::Base.transaction do
+      add_reference :protocols, :primary_pi, after: :long_title, index: true
       missing_pis = []
       new_or_found_users = {}
       not_found_users = {}
@@ -21,7 +20,7 @@ class MovePrimaryPiAssociation < ActiveRecord::Migration[5.1]
         elsif user = new_or_found_users[primary_pi.name]
           protocol.update_attribute(:primary_pi_id, user.id)
         else
-          if !not_found_users[primary_pi.name] && (ldap_results = LdapSearch.new.info_query(primary_pi.first_name) & LdapSearch.new.info_query(primary_pi.last_name)) && ldap_results.count == 1 
+          if !not_found_users[primary_pi.name] && (ldap_results = LdapSearch.new.info_query(primary_pi.first_name) & LdapSearch.new.info_query(primary_pi.last_name)) && ldap_results.count == 1
             record = ldap_results.first
             user = User.where(email: record[:email]).first_or_create(password: Devise.friendly_token[0,20], net_id: record[:netid], name: [record[:first_name], record[:last_name]].join(' '), first_name: record[:first_name], last_name: record[:last_name], middle_initial: record[:middle_initial], pvid: record[:pvid])
             new_or_found_users[user.name] = user # Cache the user for future calls to avoid repeated LDAP checks
