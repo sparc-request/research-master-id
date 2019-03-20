5c07cb74efc42cbcc99364fb5e370182b7eccd85
Andrew Cates <amcates@gmail.com>
2019-03-20 14:47:54 -0400

    fixing small issue with protocol model

--- app/models/protocol.rb
+++ app/models/protocol.rb
@@ -1,7 +1,7 @@
 class Protocol < ApplicationRecord
   audited
   self.inheritance_column = nil
-  has_one :primary_pi, class_name: :user
+  belongs_to :primary_pi, class_name: 'User'
   has_many :research_master_coeus_relations
   has_many :research_masters, through: :research_master_coeus_relations
 end
