c2d57ba63584ce1cf1d121510446b131ce485ae6
Kyle Glick <glickk@musc.edu>
2019-03-21 12:42:16 -0500

    use APIs to find primary PIs

--- app/models/protocol.rb
+++ app/models/protocol.rb
@@ -1,7 +1,9 @@
 class Protocol < ApplicationRecord
   audited
   self.inheritance_column = nil
-  has_one :primary_pi, class_name: :user
+
+  belongs_to :primary_pi, class_name: :user
+
   has_many :research_master_coeus_relations
   has_many :research_masters, through: :research_master_coeus_relations
 end
