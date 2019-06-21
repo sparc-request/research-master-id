6c94c8c6895c026d5e97230579d7101f4a28221b
Stuart Johnson <work.stuart.johnson@gmail.com>
2019-06-21 17:20:16 -0400

    basic implementation of page listing removed rmids [#166613328]

--- app/models/deleted_rmid.rb
+++ app/models/deleted_rmid.rb
@@ -1,3 +1,5 @@
 class DeletedRmid < ApplicationRecord
   belongs_to :user
+  belongs_to :creator, class_name: 'User', foreign_key: 'creator_id'
+  belongs_to :pi, class_name: 'User', foreign_key: 'pi_id'
 end
