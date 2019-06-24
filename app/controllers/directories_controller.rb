cda65b24c7b15d445c0df1c97b4ae5dcf934ab06
Stuart Johnson <work.stuart.johnson@gmail.com>
2019-06-24 10:00:16 -0400

    small tweaks

--- app/controllers/directories_controller.rb
+++ app/controllers/directories_controller.rb
@@ -2,7 +2,7 @@ class DirectoriesController < ApplicationController
 
   def show
     ldap_search = LdapSearch.new
-    @user_info = ldap_search.info_query(params[:name])
+    @user_info = ldap_search.info_query(params[:name].chomp(" "))
   end
 end
 
