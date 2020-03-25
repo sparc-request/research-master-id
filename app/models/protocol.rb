14ab9b05b6dcc5e781b2f72b269aa19ba8d7ba53
Stuart Johnson <work.stuart.johnson@gmail.com>
2020-03-25 09:32:52 -0400

    quick fix for association validation

--- app/models/protocol.rb
+++ app/models/protocol.rb
@@ -2,7 +2,7 @@ class Protocol < ApplicationRecord
   audited
   self.inheritance_column = nil
 
-  belongs_to :primary_pi, class_name: 'User'
+  belongs_to :primary_pi, class_name: 'User', optional: true
 
   has_many :research_master_coeus_relations
   has_many :research_masters, through: :research_master_coeus_relations
