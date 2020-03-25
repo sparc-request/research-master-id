466fb5a01666ed25f0e7a60ab9d86b25fd4297dd
Stuart Johnson <work.stuart.johnson@gmail.com>
2020-03-25 09:49:32 -0400

    typo fixes, etc

--- app/models/protocol.rb
+++ app/models/protocol.rb
@@ -5,8 +5,8 @@ class Protocol < ApplicationRecord
   belongs_to :primary_pi, class_name: 'User', optional: true
 
   has_many :research_master_coeus_relations
-  has_many :research_masters, through: :research_master_coeus_relations
+  # has_many :research_masters, through: :research_master_coeus_relations
 
   has_many :research_master_cayuse_relations
-  has_many :research_masters, through: :research_master_cayuse_relations
+  # has_many :research_masters, through: :research_master_cayuse_relations
 end
