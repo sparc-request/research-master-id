d2062dfd5809eb3d3c5d85556087be13e2609793
Stuart Johnson <work.stuart.johnson@gmail.com>
2020-08-28 11:38:22 -0400

    migration updates

--- db/migrate/20200828152243_change_long_title_on_deleted_rmids_to_text.rb
+++ db/migrate/20200828152243_change_long_title_on_deleted_rmids_to_text.rb
@@ -1,5 +1,6 @@
 class ChangeLongTitleOnDeletedRmidsToText < ActiveRecord::Migration[5.2]
   def change
+    change_column_default :deleted_rmids, :long_title, nil
     change_column :deleted_rmids, :long_title, :text
   end
 end
