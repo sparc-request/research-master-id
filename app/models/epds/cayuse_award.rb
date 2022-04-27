ca07c607ea5feb137e03bee581dc07cf8ac2adcf
Greg Mathias <mathiasgreg@gmail.com>
2022-04-27 16:39:22 -0400

    Add ePDS DB connection and update Coeus rake task

---
+++ app/models/epds/cayuse_award.rb
@@ -0,0 +1,7 @@
+class CayuseAward < Epds::Connection
+  self.table_name = 'SRC_CAYUSE_AWARDS'
+  self.primary_key = 'AWARD_NUMBER'
+
+  belongs_to :cayuse_project, foreign_key: 'PROJECT_ID'
+  has_many :cayuse_research_teams, foreign_key: 'AWARD_NUMBER'
+end
---
+++ app/models/epds/cayuse_research_team.rb
@@ -0,0 +1,5 @@
+class CayuseResearchTeam < Epds::Connection
+  self.table_name = 'SRC_CAYUSE_RESEARCH_TEAM'
+  self.primary_key = 'PID'
+  belongs_to :cayuse_award, foreign_key: 'AWARD_NUMBER'
+end
