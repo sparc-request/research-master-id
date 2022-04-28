a71c0629861d174e1fe58d22e8797300716b1211
Greg Mathias <mathiasgreg@gmail.com>
2022-04-28 18:41:28 -0400

    Fix epds module class nanmes

--- app/models/epds/cayuse_research_team.rb
+++ app/models/epds/cayuse_research_team.rb
@@ -1,4 +1,4 @@
-class CayuseResearchTeam < Epds::Connection
+class Epds::CayuseResearchTeam < Epds::Connection
   self.table_name = 'SRC_CAYUSE_RESEARCH_TEAM'
   self.primary_key = 'PID'
   belongs_to :cayuse_award, foreign_key: 'AWARD_NUMBER'
